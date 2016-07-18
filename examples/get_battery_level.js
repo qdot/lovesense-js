151ee99d6c8161651a325a377e3dbd21cacad881
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-17 18:11:07 -0700

    chore: Run vibration from example

--- examples/get_battery_level.js
+++ examples/get_battery_level.js
@@ -7,6 +7,7 @@ const async = require('marcosc-async');
 args
   .version('0.1.0')
   .option('-p, --port <port>', 'Address of serial port to use')
+  .option('-v, --vibrate [vibrate]', 'Speed to vibrate at', parseInt)
   .parse(process.argv);
 
 if (args.port === undefined) {
@@ -15,8 +16,16 @@ if (args.port === undefined) {
 
 let l = new lovesense.LovesenseSerial(args.port);
 async.task(function*() {
+  console.log("opening!");
   yield l.open();
-  let level = yield l.batteryLevel();
-  console.log('Battery Level: ' + level + '%');
+  if (args.vibrate !== undefined) {
+    console.log("setting vibrate speed!");
+    l.vibrate(args.vibrate);
+  } else {
+    console.log("waiting for battery level!");
+    let level = yield l.batteryLevel();
+    console.log('Battery Level: ' + level + '%');
+  }
   yield l.close();
+  console.log("closed!");
 });
