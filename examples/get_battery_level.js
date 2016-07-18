57426ab8c6fdcb14c478a0d0e522ca5fc873d457
Kyle Machulis <kyle@nonpolynomial.com>
2016-07-18 10:47:35 -0700

    chore: More example features

--- examples/get_battery_level.js
+++ examples/get_battery_level.js
@@ -8,6 +8,8 @@ args
   .version('0.1.0')
   .option('-p, --port <port>', 'Address of serial port to use')
   .option('-v, --vibrate [vibrate]', 'Speed to vibrate at', parseInt)
+  .option('-r, --rotate [rotate]', 'Speed to rotate at', parseInt)
+  .option('-a, --air [air]', 'Air level to move to', parseInt)
   .parse(process.argv);
 
 if (args.port === undefined) {
@@ -16,16 +18,25 @@ if (args.port === undefined) {
 
 let l = new lovesense.LovesenseSerial(args.port);
 async.task(function*() {
-  console.log("opening!");
+  console.log('opening!');
   yield l.open();
   if (args.vibrate !== undefined) {
-    console.log("setting vibrate speed!");
+    console.log('setting vibrate speed!');
     l.vibrate(args.vibrate);
-  } else {
-    console.log("waiting for battery level!");
+  }
+  if (args.rotate !== undefined) {
+    console.log('setting vibrate speed!');
+    l.rotate(args.rotate);
+  }
+  if (args.air !== undefined) {
+    console.log('setting air level!');
+    l.airlevel(args.air);
+  }
+  if (args.battery !== undefined) {
+    console.log('waiting for battery level!');
     let level = yield l.batteryLevel();
     console.log('Battery Level: ' + level + '%');
   }
   yield l.close();
-  console.log("closed!");
+  console.log('closed!');
 });
